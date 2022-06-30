9c610aa18eb4755acf909304de34b69f561e5cce
Daniel Quintero <daniel.manny10@hotmail.com>
2022-06-30 18:41:38 -0500

    install redux

--- componentes/Item.jsx
+++ componentes/Item.jsx
@@ -2,59 +2,61 @@ import React,{useState} from 'react'
 import { View,Text, FlatList, TouchableOpacity} from 'react-native'
 import styles from '../styles';
 import ItemWrapper from './ItemWrapper'
+import {resultados} from '../sources/data';
 
 const Item = ({navigation}) => {
-    const [data, setData] = useState([
-        {
-            id:1,
-            nombre:'Celular Samsung',
-            precio:20000,
-            imagen:'https://www.celularesytelefonos.com/wcsstore/GlobalSolutions/images/catalog/Samsung/Galaxy-S10-Plus-64GB-Neg',
-            descripcion:'Celular Samsung Galaxy S10 Plus 64GB'
-        },{
-            id:2,
-            nombre:'Celular Apple',
-            precio:30000,
-            imagen:'https://www.celularesytelefonos.com/wcsstore/GlobalSolutions/images/catalog/Apple/iPhone-11-Pro-Max-256GB-Neg',
-            descripcion:'Celular Apple iPhone 11 Pro Max 256GB'
-        },{
-            id:3,
-            nombre:'Celular Google',
-            precio:40000,
-            imagen:'https://www.celularesytelefonos.com/wcsstore/GlobalSolutions/images/catalog/Google/Pixel-3-XL-128GB-Neg',
-            descripcion:'Celular Google Pixel 3 XL 128GB'
-        },{
-            id:4,
-            nombre:'Celular OnePlus',
-            precio:50000,
-            imagen:'https://www.celularesytelefonos.com/wcsstore/GlobalSolutions/images/catalog/OnePlus/OnePlus-7-Pro-256GB-Neg',
-            descripcion:'Celular OnePlus 7 Pro 256GB'
-        },{
-            id:5,
-            nombre:'Celular Nokia',
-            precio:60000,
-            imagen:'https://www.celularesytelefonos.com/wcsstore/GlobalSolutions/images/catalog/Nokia/Nokia-X-1-128GB-Neg',
-            descripcion:'Celular Nokia X1 128GB'
-        },{
-            id:6,
-            nombre:'Celular Samsung',
-            precio:70000,
-            imagen:'https://www.celularesytelefonos.com/wcsstore/GlobalSolutions/images/catalog/Samsung/Galaxy-S10-Plus-64GB-Neg',
-            descripcion:'Celular Samsung Galaxy S10 Plus 64GB'
-        },{
-            id:7,
-            nombre:'Celular Apple',
-            precio:80000,
-            imagen:'https://www.celularesytelefonos.com/wcsstore/GlobalSolutions/images/catalog/Apple/iPhone-11-Pro-Max-256GB-Neg',
-            descripcion:'Celular Apple iPhone 11 Pro Max 256GB'
-        },{
-            id:8,
-            nombre:'Celular Google',
-            precio:90000,
-            imagen:'https://www.celularesytelefonos.com/wcsstore/GlobalSolutions/images/catalog/Google/Pixel-3-XL-128GB-Neg',
-            descripcion:'Celular Google Pixel 3 XL 128GB'
-        }
-    ]);
+    // const [data, setData] = useState(resultados)
+    // const [data, setData] = useState([
+    //     {
+    //         id:1,
+    //         nombre:'Celular Samsung',
+    //         precio:20000,
+    //         imagen:'https://www.celularesytelefonos.com/wcsstore/GlobalSolutions/images/catalog/Samsung/Galaxy-S10-Plus-64GB-Neg',
+    //         descripcion:'Celular Samsung Galaxy S10 Plus 64GB'
+    //     },{
+    //         id:2,
+    //         nombre:'Celular Apple',
+    //         precio:30000,
+    //         imagen:'https://www.celularesytelefonos.com/wcsstore/GlobalSolutions/images/catalog/Apple/iPhone-11-Pro-Max-256GB-Neg',
+    //         descripcion:'Celular Apple iPhone 11 Pro Max 256GB'
+    //     },{
+    //         id:3,
+    //         nombre:'Celular Google',
+    //         precio:40000,
+    //         imagen:'https://www.celularesytelefonos.com/wcsstore/GlobalSolutions/images/catalog/Google/Pixel-3-XL-128GB-Neg',
+    //         descripcion:'Celular Google Pixel 3 XL 128GB'
+    //     },{
+    //         id:4,
+    //         nombre:'Celular OnePlus',
+    //         precio:50000,
+    //         imagen:'https://www.celularesytelefonos.com/wcsstore/GlobalSolutions/images/catalog/OnePlus/OnePlus-7-Pro-256GB-Neg',
+    //         descripcion:'Celular OnePlus 7 Pro 256GB'
+    //     },{
+    //         id:5,
+    //         nombre:'Celular Nokia',
+    //         precio:60000,
+    //         imagen:'https://www.celularesytelefonos.com/wcsstore/GlobalSolutions/images/catalog/Nokia/Nokia-X-1-128GB-Neg',
+    //         descripcion:'Celular Nokia X1 128GB'
+    //     },{
+    //         id:6,
+    //         nombre:'Celular Samsung',
+    //         precio:70000,
+    //         imagen:'https://www.celularesytelefonos.com/wcsstore/GlobalSolutions/images/catalog/Samsung/Galaxy-S10-Plus-64GB-Neg',
+    //         descripcion:'Celular Samsung Galaxy S10 Plus 64GB'
+    //     },{
+    //         id:7,
+    //         nombre:'Celular Apple',
+    //         precio:80000,
+    //         imagen:'https://www.celularesytelefonos.com/wcsstore/GlobalSolutions/images/catalog/Apple/iPhone-11-Pro-Max-256GB-Neg',
+    //         descripcion:'Celular Apple iPhone 11 Pro Max 256GB'
+    //     },{
+    //         id:8,
+    //         nombre:'Celular Google',
+    //         precio:90000,
+    //         imagen:'https://www.celularesytelefonos.com/wcsstore/GlobalSolutions/images/catalog/Google/Pixel-3-XL-128GB-Neg',
+    //         descripcion:'Celular Google Pixel 3 XL 128GB'
+    //     }
+    // ]);
         const numColumns = 2;
         const oneItem=(item)=>{navigation.navigate('ItemDetail',{
             id:item.id,
@@ -91,7 +93,7 @@ const Item = ({navigation}) => {
     <FlatList
         style={styles.itemContainer}
         numColumns={numColumns}
-        data={data}
+        data={resultados}
         renderItem={renderItem}
         keyExtractor={(item) => item.id.toString()}
     />
---
+++ sources/data.js
@@ -0,0 +1,51 @@
+export const resultados=[
+    {
+        id:1,
+        nombre:'Celular Samsung',
+        precio:20000,
+        imagen:'https://www.celularesytelefonos.com/wcsstore/GlobalSolutions/images/catalog/Samsung/Galaxy-S10-Plus-64GB-Neg',
+        descripcion:'Celular Samsung Galaxy S10 Plus 64GB'
+    },{
+        id:2,
+        nombre:'Celular Apple',
+        precio:30000,
+        imagen:'https://www.celularesytelefonos.com/wcsstore/GlobalSolutions/images/catalog/Apple/iPhone-11-Pro-Max-256GB-Neg',
+        descripcion:'Celular Apple iPhone 11 Pro Max 256GB'
+    },{
+        id:3,
+        nombre:'Celular Google',
+        precio:40000,
+        imagen:'https://www.celularesytelefonos.com/wcsstore/GlobalSolutions/images/catalog/Google/Pixel-3-XL-128GB-Neg',
+        descripcion:'Celular Google Pixel 3 XL 128GB'
+    },{
+        id:4,
+        nombre:'Celular OnePlus',
+        precio:50000,
+        imagen:'https://www.celularesytelefonos.com/wcsstore/GlobalSolutions/images/catalog/OnePlus/OnePlus-7-Pro-256GB-Neg',
+        descripcion:'Celular OnePlus 7 Pro 256GB'
+    },{
+        id:5,
+        nombre:'Celular Nokia',
+        precio:60000,
+        imagen:'https://www.celularesytelefonos.com/wcsstore/GlobalSolutions/images/catalog/Nokia/Nokia-X-1-128GB-Neg',
+        descripcion:'Celular Nokia X1 128GB'
+    },{
+        id:6,
+        nombre:'Celular Samsung',
+        precio:70000,
+        imagen:'https://www.celularesytelefonos.com/wcsstore/GlobalSolutions/images/catalog/Samsung/Galaxy-S10-Plus-64GB-Neg',
+        descripcion:'Celular Samsung Galaxy S10 Plus 64GB'
+    },{
+        id:7,
+        nombre:'Celular Apple',
+        precio:80000,
+        imagen:'https://www.celularesytelefonos.com/wcsstore/GlobalSolutions/images/catalog/Apple/iPhone-11-Pro-Max-256GB-Neg',
+        descripcion:'Celular Apple iPhone 11 Pro Max 256GB'
+    },{
+        id:8,
+        nombre:'Celular Google',
+        precio:90000,
+        imagen:'https://www.celularesytelefonos.com/wcsstore/GlobalSolutions/images/catalog/Google/Pixel-3-XL-128GB-Neg',
+        descripcion:'Celular Google Pixel 3 XL 128GB'
+    }
+]
